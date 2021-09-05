ef97e97afcb9817fffe350ffb602e8cd8d63f7ec
ka-zuu <kazunari.tabei@gmail.com>
2021-09-05 17:47:45 +0900

    DHT22が値の取得失敗することがあるので、リトライを追加

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 14
+# 14番ピンをセット
 instance = dht22.DHT22(pin=14)
 
-# output value a time
-result = instance.read()
-if result.is_valid():
-    msg = u"{0:0.1f}℃ {1:0.1f}％".format(result.temperature, result.humidity)
-else:
-    msg = "err"
+# 値の取得に失敗することがあるので、2秒インターバルで5回ループする
+count = 0
+while count < 5:
+    result = instance.read()
+    if result.is_valid():
+        msg = u"{0:0.1f}℃ {1:0.1f}％".format(result.temperature, result.humidity)
+        count = 9999
+    else:
+        msg = "err"
+        count += 1
+        time.sleep(2)
 
 # make html
 f = open('index.html', 'w')
